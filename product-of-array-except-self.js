2aead216c4c868f186b9a1382264f058350791a1
Dhruv Jain <dhruvjainpenny@gmail.com>
2022-05-22 13:00:24 +0530

    :fire: Product of Array Except Self

---
+++ product-of-array-except-self.js
@@ -0,0 +1,17 @@
+const productExceptSelf = (nums) => {
+  const result = new Array(nums.length).fill(1);
+
+  let prefix = 1;
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = prefix;
+    prefix *= nums[i];
+  }
+
+  let postfix = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= postfix;
+    postfix *= nums[i];
+  }
+
+  return result;
+};
